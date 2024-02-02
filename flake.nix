ae57002930226a8ffab228ac2492a0a567c604dd
kpbaks <kristoffer.pbs@tuta.io>
2024-02-02 10:49:55 +0100

    feat(flake): create `flake.nix` with typst tools in

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Computer Engineering Master Thesis";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.inputs.flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in
+      with pkgs; {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+          buildInputs = [
+            typst
+            typstfmt
+            typst-lsp
+            graphviz
+            python3
+            just
+          ];
+        };
+      });
+}
