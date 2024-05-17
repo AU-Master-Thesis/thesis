8a425ab393bfc0bb6e4327cd7ea17ed7ef93a8ee
kpbaks <kristoffer.pbs@gmail.com>
2024-05-17 09:48:11 +0200

    fix(scripts): forgot to rename to new script

--- scripts/watch-compile-all-figures.fish
+++ scripts/watch-compile-all-figures.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i fish -p watchexec
 
-watchexec --clear --watch ./figures -e typ -e tex ./scripts/tectonic-compile-all.fish
+watchexec --clear --watch ./figures -e typ -e tex ./scripts/compile-all-figures.fish
